e5c96a8aa6d1b828bf2eee24e74f50dd437c4228
Goto-star <goyu620@gmail.com>
2023-04-26 23:43:07 +0900

    勤怠打刻機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :attendances, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 20 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def today_attendance
+    Attendance.where(created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day, user_id: id).first
+  end
 end
