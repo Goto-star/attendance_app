2c0d6dd091a8d155e44b4c75cef3241a9e16d90b
Goto-star <goyu620@gmail.com>
2023-04-27 02:40:31 +0900

    Heroku Dails Update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_26_173519) do
+ActiveRecord::Schema.define(version: 2023_04_24_124406) do
 
   create_table "attendances", force: :cascade do |t|
     t.datetime "work_start_time"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2023_04_26_173519) do
     t.string "company_name"
     t.integer "company_id"
     t.integer "employ_id"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
