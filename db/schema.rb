50fb0bf4de3bc3a5d466f0d272f2952986f0f5fd
Goto-star <goyu620@gmail.com>
2023-04-27 15:26:33 +0900

    render add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_24_124406) do
+ActiveRecord::Schema.define(version: 2023_04_27_062612) do
 
   create_table "attendances", force: :cascade do |t|
     t.datetime "work_start_time"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2023_04_24_124406) do
     t.string "company_name"
     t.integer "company_id"
     t.integer "employ_id"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
